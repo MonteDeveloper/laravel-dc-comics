e652fcef9a3f443cbe277a577557791ef0c04d93
MonteDeveloper <lorenzomontello83@gmail.com>
2023-07-05 10:32:19 +0200

    comic show

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -7,9 +7,9 @@
         <div class="d-flex flex-wrap">
             @foreach ($comics as $comic)
             <div class="p-3 col-3">
-                <div class="card my-card h-100">
+                <div class="card my-card h-100" onclick="window.location = '{{ route('comics.show', $comic->id) }}'">
                     <img src="{{$comic->thumb}}" class="card-img-top" alt="comicImg">
-                    <div class="card-body d-flex align-items-center justify-content-center">
+                    <div class="card-body d-flex flex-column align-items-center justify-content-center">
                         <h5 class="card-title text-center">{{$comic->title}}</h5>
                     </div>
                 </div>    
---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container my-3">
+    <h1>Comic Info</h1>
+    <div class="container d-flex flex-column justify-content-center">
+        <div class="card w-100">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="{{$comic->thumb}}" class="img-fluid rounded-start" alt="comicImg">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$comic->title}}</h5>
+                        <p class="card-text"><strong>Descrizione: </strong>{{$comic->description}}</p>
+                        <p class="card-text"><strong>Artisti: </strong><small class="text-muted">{{$comic->artists}}</small></p>
+                        <p class="card-text"><strong>Scrittori: </strong><small class="text-muted">{{$comic->writers}}</small></p>
+                        <p class="card-text"><strong>Prezzo: </strong>${{$comic->price}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="btn btn-primary m-3" onclick="window.location = '{{ route('comics.index')}}'">TORNA ALLA HOMEPAGE</button>
+    </div>
+
+</div>
+@endsection
